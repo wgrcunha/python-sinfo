406ef460e87c26e88ef944c5a3f3683c224cfa28
Francisco Freire <francisco.freire@locaweb.com.br>
2011-09-09 07:09:11 -0300

    Added statfs to mounts

--- src/lib/Linux.py
+++ src/lib/Linux.py
@@ -34,6 +34,19 @@ def mounts():
                     'fstype': line[2],
                     'device': line[0]
                 }
+                try:
+                    stat = os.statvfs(line[1])
+                except Exception, e:
+                    print "Exception on fs: %s (%s)" % (line[1], str(e))
+                else:
+                    data[line[1]].update({
+                        'f_blocks': stat.f_blocks,
+                        'f_bfree': stat.f_bfree,
+                        'f_bavail': stat.f_bavail,
+                        'f_files': stat.f_files,
+                        'f_ffree': stat.f_ffree,
+                        'f_favail': stat.f_favail,
+                    })
     return data
 
 ALL = [ hostinfo, cpuinfo, meminfo, mounts ]
